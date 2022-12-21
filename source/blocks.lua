438275095517f6ddc516ad8570148c0409abc9b6
Glen McCann <glenmccann2@gmail.com>
2022-12-21 15:49:45 -0500

    changed style of blocks

--- source/blocks.lua
+++ source/blocks.lua
@@ -4,7 +4,7 @@ import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/ui"
 import "CoreLibs/crank"
-import "CoreLibs/nineslice"
+-- import "CoreLibs/nineslice"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -17,8 +17,8 @@ gridview:setNumberOfRows(1)
 gridview:setNumberOfColumns(#blocks)
 gridview:setCellPadding(2, 2, 2, 2)
 
-gridview.backgroundImage = gfx.nineSlice.new("images/gridBackground", 7, 7, 18, 18)
-gridview:setContentInset(5, 5, 5, 5)
+-- gridview.backgroundImage = gfx.nineSlice.new("images/gridBackground", 7, 7, 18, 18)
+-- gridview:setContentInset(5, 5, 5, 5)
 
 local gridViewSprite = gfx.sprite.new()
 gridViewSprite:setCenter(0, 0)
@@ -30,6 +30,7 @@ function gridview:drawCell(section, row, column, selected, x, y, width, height)
         gfx.fillRoundRect(x, y, width, height, 4)
         gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     else
+        gfx.drawRoundRect(x, y, width, height, 4)
         gfx.setImageDrawMode(gfx.kDrawModeCopy)
     end
     local fontHeight = gfx.getSystemFont():getHeight()
