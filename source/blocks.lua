53ba98b21d9297b95cb2d2f5f57bac97aca1394a
Glen McCann <glenmccann2@gmail.com>
2022-12-20 19:22:55 -0500

    Grid moved to top and changed to blocks for standard game

--- source/blocks.lua
+++ source/blocks.lua
@@ -9,11 +9,12 @@ import "CoreLibs/nineslice"
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-local gridview = pd.ui.gridview.new(0, 32)
+local gridview = pd.ui.gridview.new(32, 48)
 
-local fruits = {"apple", "banana", "mango", "kiwi", "pineapple", "cherry", "orange"}
+local blocks = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 
-gridview:setNumberOfRows(#fruits)
+gridview:setNumberOfRows(1)
+gridview:setNumberOfColumns(#blocks)
 gridview:setCellPadding(2, 2, 2, 2)
 
 gridview.backgroundImage = gfx.nineSlice.new("images/gridBackground", 7, 7, 18, 18)
@@ -21,7 +22,7 @@ gridview:setContentInset(5, 5, 5, 5)
 
 local gridViewSprite = gfx.sprite.new()
 gridViewSprite:setCenter(0, 0)
-gridViewSprite:moveTo(100, 70)
+gridViewSprite:moveTo(32, 8)
 gridViewSprite:add()
 
 function gridview:drawCell(section, row, column, selected, x, y, width, height)
@@ -32,27 +33,27 @@ function gridview:drawCell(section, row, column, selected, x, y, width, height)
         gfx.setImageDrawMode(gfx.kDrawModeCopy)
     end
     local fontHeight = gfx.getSystemFont():getHeight()
-    gfx.drawTextInRect(fruits[row], x, y + (height/2) - fontHeight/2 + 2, width, height, nil, nil, kTextAlignment.center)
+    gfx.drawTextInRect(blocks[column], x, y + (height/2) - fontHeight/2 + 2, width, height, nil, nil, kTextAlignment.center)
 end
 
-function updateNumbers()
-    if pd.buttonJustPressed(pd.kButtonUp) then
-        gridview:selectPreviousRow(true)
-    elseif pd.buttonJustPressed(pd.kButtonDown) then
-        gridview:selectNextRow(true)
+function updateBlocks()
+    if pd.buttonJustPressed(pd.kButtonLeft) then
+        gridview:selectPreviousColumn(true)
+    elseif pd.buttonJustPressed(pd.kButtonRight) then
+        gridview:selectNextColumn(true)
     end
 
     local crankTicks = pd.getCrankTicks(2)
     if crankTicks == 1 then
-        gridview:selectNextRow(true)
+        gridview:selectNextColumn(true)
     elseif crankTicks == -1 then
-        gridview:selectPreviousRow(true)
+        gridview:selectPreviousColumn(true)
     end
 
     if gridview.needsDisplay then
-        local gridviewImage = gfx.image.new(200, 100)
+        local gridviewImage = gfx.image.new(336, 64)
         gfx.pushContext(gridviewImage)
-            gridview:drawInRect(0, 0, 200, 100)
+            gridview:drawInRect(0, 0, 336, 64)
         gfx.popContext()
         gridViewSprite:setImage(gridviewImage)
     end
--- source/main.lua
+++ source/main.lua
@@ -3,7 +3,7 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
-import "numbers"
+import "blocks"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -12,7 +12,7 @@ local function initialize()
 end
 
 function playdate.update()
-    updateNumbers()
+    updateBlocks()
     gfx.sprite.update()
     pd.timer.updateTimers()
 end
